df0f317582e66fa0f2fc2d2da38a30d8ae6b8f5b
KEMMEGNE EMERICK <kemmegneemerick@gmail.com>
2023-09-17 10:48:44 +0100

    release-fonctionnality

--- lib/category/presentation/pages/category_detail_screen.dart
+++ lib/category/presentation/pages/category_detail_screen.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:madeincameroon/category/data/models/category_model.dart';
 import 'package:madeincameroon/shared/presentation/views/elevated_button_back.dart';
 import 'package:madeincameroon/category/presentation/views/build_filter_product.dart';
+import '../../../locator.dart';
+import '../../../product/logic/product_cubit.dart';
+import '../../../product/logic/product_state.dart';
 import '../../../product/presentation/views/gridview_product.dart';
 import '../../../shared/data/load_data.dart';
 import '../../../settings.dart';
@@ -77,7 +82,99 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
             child: ListView(
               shrinkWrap: true,
               children: [
-                GridViewProduct(listProduct: listProducts),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: BlocBuilder<ProductCubit, ProductState>(
+                    bloc: getIt.get<ProductCubit>(),
+                    buildWhen: (previousState, currentState) {
+                      return previousState.listProducts?.length !=
+                          currentState.listProducts?.length;
+                    },
+                    builder: (context, state) {
+                      if (state is ProductLoading) {
+                        return CupertinoActivityIndicator();
+                      }
+                      if (state is ProductFailure) {
+                        return Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  getIt.get<ProductCubit>().getProducts(index: 1);
+                                },
+                                icon: const Icon(
+                                  Icons.refresh,
+                                  size: 30,
+                                ),
+                              ),
+                              const SizedBox(height: 30),
+                              //Text("${state.message}"),
+                            ],
+                          ),
+                        );
+                      }
+                      if (state is ProductSuccess){
+                        print("load product success");
+                        if (state.listProducts != null) {
+                          print("state.listProducts : ${state.listProducts!.map((e) =>e.name)}");
+                          return Column(
+                            children: [
+                              GridViewProduct(listProduct: state.listProducts!),
+                              const SizedBox(
+                                height: 8.0,
+                              ),
+                              (state.numberNextPage != null)
+                                  ? Padding(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    print("++++++number for next page : ${state.numberNextPage}");
+                                    getIt
+                                        .get<ProductCubit>()
+                                        .getProducts(index: state.numberNextPage!);
+
+                                    print("------number for next page : ${state.numberNextPage}");
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.grey,
+                                    elevation: 2,
+                                    padding: EdgeInsets.all(8.0),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.refresh,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(
+                                        width: 8.0,
+                                      ),
+                                      Text(
+                                        "Voir plus",
+                                        style: TextStyle(color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                                  : Container(),
+                            ],
+                          );
+                        }
+                      }
+
+                      return Container();
+                    },
+                  ),
+                ),
                 SizedBox(
                   height: 32.0,
                 )
